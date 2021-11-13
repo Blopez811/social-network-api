a066779d0ce80bfdd35aa550e7d41af1081dec29
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-13 11:13:41 -0600

    user get all, get one, and post routes work

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -19,4 +19,32 @@ router.route('/')
 
     )
 
+router.get('/:id', (req, res) => {
+    User.findOne({ _id: req.params.id})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.route('/').post(
+    ({ body }, res) => {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    }
+)
+
 module.exports = router;
