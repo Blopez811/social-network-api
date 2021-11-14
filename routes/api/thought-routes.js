529122e572bf1d01f99a548ea8887deeac087621
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-14 13:50:46 -0600

    added thought routes, not currently working

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -11,4 +11,35 @@ router.get('/', (req, res) => {
         });
 });
 
+// get a single thought
+router.get('/:id', ({ params }, res) => {
+    Thought.findOne({ _id: params.id })
+    .select('-__v')
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id!' });
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });      
+});
+
+// create a thought
+router.post('/', ({ body }, res) => {
+    Thought.create(body)
+    .then(({body}) => {
+        return Thought.findOneAndUpdate(
+            { _id: body.userId },
+            { $push: { thought: _id } },
+            { new: true }
+        );
+    })
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => res.status(400).json(err));  
+})
+
 module.exports = router;
